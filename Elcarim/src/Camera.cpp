bf12f7fb550c4c8357478da225c53573e44d60cc
Henrik <hsn95@live.com>
2019-11-23 07:14:31 +0100

    Just learned what constexpr is :P

--- Elcarim/src/Camera.cpp
+++ Elcarim/src/Camera.cpp
@@ -5,29 +5,4 @@
 namespace Elcarim::Objects {
 	const float Camera::PROJECTION_RESOLUTION_WIDTH = 512.0f;
 	const float Camera::PROJECTION_RESOLUTION_HEIGHT = 288.0f;
-
-	glm::vec2 Camera::getUpperLeftCorner() {
-		return glm::vec2(-PROJECTION_RESOLUTION_WIDTH, PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
-	}
-	glm::vec2 Camera::getLowerLeftCorner() {
-		return glm::vec2(-PROJECTION_RESOLUTION_WIDTH, -PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
-	}
-	glm::vec2 Camera::getUpperRightCorner() {
-		return glm::vec2(PROJECTION_RESOLUTION_WIDTH, PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
-	}
-	glm::vec2 Camera::getLowerRightCorner() {
-		return glm::vec2(PROJECTION_RESOLUTION_WIDTH, -PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
-	}
-	glm::vec2 Camera::getUpperEdge() {
-		return glm::vec2(0.0f, PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
-	}
-	glm::vec2 Camera::getLowerEdge() {
-		return glm::vec2(0.0f, -PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
-	}
-	glm::vec2 Camera::getLeftEdge() {
-		return glm::vec2(-PROJECTION_RESOLUTION_WIDTH, 0.0f) / 2.0f;
-	}
-	glm::vec2 Camera::getRightEdge() {
-		return glm::vec2(PROJECTION_RESOLUTION_WIDTH, 0.0f) / 2.0f;
-	}
 }
--- Elcarim/src/Camera.hpp
+++ Elcarim/src/Camera.hpp
@@ -8,13 +8,29 @@ namespace Elcarim::Objects {
 		static const float PROJECTION_RESOLUTION_WIDTH;
 		static const float PROJECTION_RESOLUTION_HEIGHT;
 
-		static glm::vec2 getUpperLeftCorner();
-		static glm::vec2 getLowerLeftCorner();
-		static glm::vec2 getUpperRightCorner();
-		static glm::vec2 getLowerRightCorner();
-		static glm::vec2 getUpperEdge();
-		static glm::vec2 getLowerEdge();
-		static glm::vec2 getLeftEdge();
-		static glm::vec2 getRightEdge();
+		static constexpr glm::vec2 getUpperLeftCorner() {
+			return glm::vec2(-PROJECTION_RESOLUTION_WIDTH, PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
+		}
+		static constexpr glm::vec2 getLowerLeftCorner() {
+			return glm::vec2(-PROJECTION_RESOLUTION_WIDTH, -PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
+		}
+		static constexpr glm::vec2 getUpperRightCorner() {
+			return glm::vec2(PROJECTION_RESOLUTION_WIDTH, PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
+		}
+		static constexpr glm::vec2 getLowerRightCorner() {
+			return glm::vec2(PROJECTION_RESOLUTION_WIDTH, -PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
+		}
+		static constexpr glm::vec2 getUpperEdge() {
+			return glm::vec2(0.0f, PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
+		}
+		static constexpr glm::vec2 getLowerEdge() {
+			return glm::vec2(0.0f, -PROJECTION_RESOLUTION_HEIGHT) / 2.0f;
+		}
+		static constexpr glm::vec2 getLeftEdge() {
+			return glm::vec2(-PROJECTION_RESOLUTION_WIDTH, 0.0f) / 2.0f;
+		}
+		static constexpr glm::vec2 getRightEdge() {
+			return glm::vec2(PROJECTION_RESOLUTION_WIDTH, 0.0f) / 2.0f;
+		}
 	};
 }
