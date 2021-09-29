fa03d89d4af91012ecd93a9681224de758c16f73
Henrik Nyhammer <hsn95@live.com>
2021-09-29 21:58:32 +0200

    Fixed broken script and made it look nice

--- setup-project-windows.bat
+++ setup-project-windows.bat
@@ -1,11 +1,48 @@
 @echo off
 
+echo   _____            ____           ____             
+echo  ^|       ^|        /        /\    ^|    \  ^|  ^|\  /^| 
+echo  ^|_____  ^|       ^|        /  \   ^|____/  ^|  ^| \/ ^| 
+echo  ^|       ^|       ^|       ^|----^|  ^|   \   ^|  ^|    ^| 
+echo  ^|_____  ^|_____   \____  ^|    ^|  ^|    \  ^|  ^|    ^| 
+echo.
+echo ----------------- Project Setup -------------------
+echo -------------------- Windows ----------------------
+echo.
+echo.
+echo ------------------------------
+echo ----- Cloning submodules -----
+echo ------------------------------
+echo.
 git submodule init
 git submodule update
-
-.\vcpkg\bootstrap-vcpkg.bat
-
+echo.
+pause
+echo.
+echo.
+echo ----------------------------
+echo ----- Setting up vcpkg -----
+echo ----------------------------
+echo.
+call vcpkg\bootstrap-vcpkg.bat
+echo.
+pause
+echo.
+echo.
+echo -----------------------------------
+echo ----- Installing dependencies -----
+echo -----------------------------------
+echo.
+echo ----- GLFW3 -----
 .\vcpkg\vcpkg install glfw3:x64-windows
+echo.
+echo ----- GLM -----
 .\vcpkg\vcpkg install glm:x64-windows
+echo.
+echo ----- GLAD -----
 .\vcpkg\vcpkg install glad[gl-api-32]:x64-windows
+echo.
+echo echo ----- STB -----
 .\vcpkg\vcpkg install stb:x64-windows
+echo.
+pause
